6f74b52826b26683bc7034ea1ae339e49fd7d118
Jimmyjtc001 <tiancheng@bcinnovationlabs.com>
2019-10-31 17:52:12 -0400

    This is my indoorair-webapp-a

---
+++ indoorair/homepage/urls.py
@@ -0,0 +1,10 @@
+"""
+homepage/urls.py
+"""
+from django.urls import path
+from . import views
+urlpatterns = [
+   path('', views.index_page, name='index_page'),
+   path('contact', views.contact_page, name='contact_page'),
+   path('api/version', views.post_version_api, name= 'version_api'),
+]
--- indoorair/homepage/views.py
+++ indoorair/homepage/views.py
@@ -1,3 +1,29 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index_page(request):
+
+    context = {
+        'login' : 11111,
+
+    }
+
+    return render(request, 'homepage/index.html', context)
+
+
+
+def contact_page(request):
+
+
+    context = {
+        'contact' : 222222,
+
+    }
+
+    return render(request, 'homepage/contact.html', context)
+
+def post_version_api(request):
+    return JsonResponse({
+    'version': 33333,
+    })
