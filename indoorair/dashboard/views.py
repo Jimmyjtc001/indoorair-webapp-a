e82da0512ac124fd0d5d9c31d8f537d55169cd02
Jimmyjtc001 <tiancheng@bcinnovationlabs.com>
2019-11-04 19:31:48 -0500

    This is my indoorair-webapp-a assignments

--- indoorair/dashboard/views.py
+++ indoorair/dashboard/views.py
@@ -1,23 +1,16 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+
 def dashboard_page(request):
-  return render(request, "dashboard/dashboard.html",{},)
-
-def dashboard_api(request):
-
-  temp_average = request.POST.get("temp_average")
-  press_average = request.POST.get("press_average")
-  co2_average = request.POST.get("co2_average")
-  tvoc_average = request.POST.get("tvoc_average")
-  humidity_average = request.POST.get("humidity_average")
+    return render(request, "dashboard/dashboard.html", {})
 
 
-  return JsonResponse({
-       "temp_average": temp_average,
-       "press_average":press_average,
-       "co2_average":co2_average,
-       "tvoc_average":tvoc_average,
-       "humidity_average":humid_average,
-
-  })
+def get_dashboard_api(request):
+    return JsonResponse({
+         'avg_temperature': 0,
+         'avg_pressure': 0,
+         'avg_co2': 0,
+         'avg_tvoc': 0,
+         'avg_humidity': 0,
+    })
